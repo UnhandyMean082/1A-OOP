f08cdea399166b80ea2d74fb3cb77e167be2237f
Aarav Agarwal <aarav02006@gmail.com>
2025-11-25 23:54:06 +0000

    Added an argumnt to the constructor.

--- Examples Paper/src/main/java/uk/ac/cam/aa2743/Section4/ConsChain.java
+++ Examples Paper/src/main/java/uk/ac/cam/aa2743/Section4/ConsChain.java
@@ -3,26 +3,30 @@ package uk.ac.cam.aa2743.Section4;
 public class ConsChain {
     public static void create() {
         System.out.println("Instantiating classes:");
-        C c = new C();
+        C c = new C(10);
+        System.out.println("Classes instantiated.\nA's data from C: " + c.data);
     }
 }
 
 class A {
-    public A() {
+    int data;
+
+    public A(int data) {
         System.out.println("A instantiated.");
+        this.data = data;
     }
 }
 
 class B extends A {
-    public B() {
-        super();
+    public B(int data) {
+        super(data);
         System.out.println("B instantiated.");
     }
 }
 
 class C extends B {
-    public C() {
-        super();
+    public C(int data) {
+        super(data);
         System.out.println("C instantiated.");
     }
 }
